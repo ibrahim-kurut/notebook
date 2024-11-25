6e44cdb10d1e623fe14b359dd80593d559490bd4
ibrahim kurut <ibrahim.kurut06@gmail.com>
2024-11-25 21:36:29 +0300

    handle add note

--- src/App.js
+++ src/App.js
@@ -2,9 +2,13 @@ import './App.css';
 import Header from './components/Header';
 import Table from './components/Table';
 
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 function App() {
   return (
     <div>
+      <ToastContainer position="top-right" theme="colored" />
       <Header />
       <Table />
     </div>
--- src/components/AddNote.jsx
+++ src/components/AddNote.jsx
@@ -1,7 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react';
 import { IoMdCloseCircle } from "react-icons/io";
+import { toast } from 'react-toastify';
 
 const AddNote = ({ setAddNote }) => {
+
+    const [title, setTitle] = useState("");
+    const [content, setContent] = useState("");
+
+    // handle add note
+    const handleAddNote = (e) => {
+        e.preventDefault();
+
+        // validation
+        if (title.trim() === "") return toast.error("title is required");
+        if (content.trim() === "") return toast.error("content is required");
+        setTitle("");
+        setContent("");
+        const fd = ({ title, content })
+
+
+        toast.success("Note added successfully");
+        console.log(fd);
+        setAddNote(false);
+    };
+
     return (
         <div className=" h-[50vh] flex justify-center items-center">
             <div className="w-3/4 rounded-md bg-gray-300 absolute top-30 mx-auto">
@@ -15,16 +37,23 @@ const AddNote = ({ setAddNote }) => {
                     </span>
                 </div>
                 <div className="px-10 pb-10">
-                    <form className=" flex flex-col gap-5">
+                    <form className="flex flex-col gap-5" onSubmit={handleAddNote}>
                         <h1 className="text-center">Add Note</h1>
-                        <input type="text" placeholder="Title"
+                        <input
+                            type="text"
+                            placeholder="Title"
                             className="outline-none py-2 px-4 rounded-md"
+                            value={title}
+                            onChange={(e) => setTitle(e.target.value)}
                         />
-                        <textarea placeholder="Content"
+                        <textarea
+                            placeholder="Content"
                             className="outline-none py-2 px-4 rounded-md"
+                            value={content}
+                            onChange={(e) => setContent(e.target.value)}
                         />
                         <button
-                            type='submit'
+                            type="submit"
                             className="bg-blue-700 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">
                             Add Note
                         </button>
@@ -32,7 +61,7 @@ const AddNote = ({ setAddNote }) => {
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default AddNote
+export default AddNote;
